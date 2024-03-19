65b119be05c8d0041bcf10b2642d7ffdbfa2202d
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2024-03-19 11:54:43 +0530

    Add files via upload

---
+++ DAY 03/src/components/LazySuspence.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import { Suspense } from "react";
+import loader from '../assets/images/loader.gif'
+import ErrorBoundary from './ErrorBoundary';
+
+
+const LazySuspense = ({component:Component, ...rest}) => {
+    return (
+        <ErrorBoundary>
+          <Suspense fallback={<img src={loader} alt='Loader'/>}>
+            <Component {...rest}/>
+        </Suspense>
+        </ErrorBoundary>
+        )
+    
+}
+
+LazySuspense.propTypes = {
+    component: PropTypes.element.isRequired
+}
+
+export default LazySuspense
---
+++ DAY 03/src/pages/user/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+
+  const data = undefined;
+  console.log(data.name)
+  
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
