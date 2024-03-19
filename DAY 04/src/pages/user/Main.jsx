c9138aad0f754aa1bb679a30810913e9ca10a833
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2024-03-19 11:57:38 +0530

    Add files via upload

---
+++ DAY 04/src/pages/user/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Main = ({children}) => {
+    return (
+        <div>
+            <aside>
+                Sidebar
+            </aside>
+            <main>
+                {children}
+            </main>
+            
+        </div>
+    )
+
+}
+
+Main.PropTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default Main
