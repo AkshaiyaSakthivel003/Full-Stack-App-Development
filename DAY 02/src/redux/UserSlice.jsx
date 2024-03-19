75054ba634c0ec68510a10b56b9d50e9f6f6aaaa
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2024-03-19 11:51:06 +0530

    Add files via upload

---
+++ DAY 02/src/redux/UserSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    isAuthenticated: false,
+    token: '',
+    role: ''
+}
+
+const UserSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setAuthenticated: (state, action) => {
+            state.isAuthenticated = action.payload
+        },
+
+        setToken: (state, action) => {
+            state.token = action.payload
+        },
+
+        setRole: (state, ation) => {
+            state.role = action.payload
+        }
+    }
+})
+
+export const {setAuthenticated, setRole, setToken} = UserSlice.actions;
+export default UserSlice.reducer;
